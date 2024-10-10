36bf7c4b60fd40d60b9bcdfe0d206b61bce8197d
Sandeep Machiraju <30957455+machirajusaisandeep@users.noreply.github.com>
2024-10-10 13:27:10 +0530

    added termsn

---
+++ src/app/policy/index.tsx
@@ -0,0 +1,11 @@
+import PrivacyPolicy from "./privacy-policy";
+import TermsConditions from "./terms-conditions";
+
+const Policy = () => {
+  return <div>
+    <PrivacyPolicy />
+    <TermsConditions />      
+  </div>;
+};
+
+export default Policy;
